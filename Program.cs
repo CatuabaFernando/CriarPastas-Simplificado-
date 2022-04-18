e3f2620f691a97433f6b0cd5acbc6bb356c76f7e
Fernando Catuaba <fernandocatuaba1@gmail.com>
2022-04-18 10:07:17 -0300

    Voltando a aplicação de console e tentando fazer funcionar em ooutros computadores.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 
-
-/***********************************************PROPRIEDADE DE SAÍDA DO PROJETO FOI ALTERADA DE CONSOLE PARA WINDOWS FORMS PARA EXECULTAR SEM ABRIR TELA PRETA***************************************/
 
 namespace TesteDeListas
 {
